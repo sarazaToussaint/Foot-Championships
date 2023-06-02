15bdac7e6812906ce37269d80143de767b52ade8
sarazaToussaint <tmsaraza@gmail.com>
2023-06-02 23:27:24 +0200

    Delete unusede files in src folder

--- src/Components/__test__/Leagues.test.js
+++ src/Components/__test__/Leagues.test.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from '../redux/configStore';
-import LiguesDatails from '../LiguesDatails';
+// import { BrowserRouter } from 'react-router-dom';
+import store from '../../Redux/store';
+import Leagues from '../Leagues';
 
 describe('Leagues test', () => {
   it('renders leagues', () => {
     const app = renderer
       .create(
         <Provider store={store}>
-          <BrowserRouter>
-            <LiguesDatails />
-          </BrowserRouter>
+          <Leagues />
         </Provider>,
       )
       .toJSON();
